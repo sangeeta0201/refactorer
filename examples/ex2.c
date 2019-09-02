5017282cb66e0898e26da3a1fde3df26558ed6df
sangeeta0201 <sc1696@scarletmail.rutgers.edu>
2019-09-02 15:29:15 -0400

    added more examples

---
+++ examples/ex2.c
@@ -0,0 +1,5 @@
+int foo(double z){
+	double x = 2.5;
+	double y = 2.5;
+	double xx = z  + x; 
+}
---
+++ examples/ex3.c
@@ -0,0 +1,8 @@
+int foo(){
+	double x = 2.3;
+	double y = 3.4; 
+	double z;
+	double sum;
+	z = x + y+ 3.1; 
+//	sum = sqrt(z);
+}
